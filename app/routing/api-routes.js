9ced485faa1b02e85f8df0fa4dc20ccc04e0ceaf
Michael Preston <michael.n.preston@gmail.com>
2019-01-31 01:17:01 -0600

    setting up the routes for api

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,1 +1,12 @@
 var friends = require("../data/friends.js");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    })
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friends); //replace w/ best match
+    })
+}
